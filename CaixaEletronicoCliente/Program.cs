951535ce823741b721bd3b6860ff3c716b29bd9b
RafaCarva <rafael.faria.carvalho@gmail.com>
2019-07-14 23:42:00 -0300

    Add a lib CaixaEletronico09

---
+++ CaixaEletronicoCliente/Program.cs
@@ -0,0 +1,16 @@
+﻿using CaixaEletronico09;
+using System;
+
+
+namespace CaixaEletronicoCliente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var caixaEletronico = new CaixaEletronico();
+
+            Console.WriteLine("-->"+caixaEletronico.cofre);
+        }
+    }
+}
